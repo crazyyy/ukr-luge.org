b9459e82c538887a11490d0d56d9f3512b9cdbd4
Vitali Antoniuk <crazyyy@gmail.com>
2017-09-06 16:50:09 +0300

    before wordpressed

---
+++ html/js/slider.js
@@ -0,0 +1,2 @@
+jQuery(document).ready(function(e){e("#post-slider").flexslider({animation:admiral_slider_params.animation,slideshowSpeed:admiral_slider_params.speed,namespace:"zeeflex-",selector:".zeeslides > li",smoothHeight:!0,pauseOnHover:!0,controlsContainer:".post-slider-controls"})});
+//# sourceMappingURL=maps/slider.js.map
